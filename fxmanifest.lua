4938d6c6a3e34d980c98a652644614159bbcee48
Tarti 💤 <76661960+TartiFloux@users.noreply.github.com>
2026-02-04 09:10:38 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Tarti'
+description 'Réduction du spawn des PNJ et du trafic zone par zone'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
